31f875b27c84e76ffff23a132816466d22e06d88
Ha Van <vannunganha@gmail.com>
2019-07-22 13:50:55 +0300

    connect to mongodb Mlab

--- utils/database.js
+++ utils/database.js
@@ -3,17 +3,19 @@
 const mysql = require("mysql2");
 
 module.exports = pool.promise(); */
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const db = require("../config/database").mongoURI;
+const mongoConnect = callback => {
+  MongoClient.connect(db, { useNewUrlParser: true })
+    .then(result => {
+      console.log("connected");
+      callback(result);
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    dialect: "mysql",
-    host: process.env.DATABASE_HOSTNAME,
-    port: process.env.DATABASE_PORT
-  }
-);
-
-module.exports = sequelize;
+module.exports = mongoConnect;
